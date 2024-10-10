5667ae43ae1b04b2c1f68dc2eea2a81ef8849724
EA - Eduardo Cerqueira Bezerra <eduardo_cerqueira@educadventista.org>
2024-10-10 13:42:32 -0300

    my first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const botao = document.querySelector('button')
+botao.addEventListener('click', soma)
+
+function soma(num, num0, num1){
+    let num1 = document.querySelector('#um')
+    let num2 = document.querySelector('#dois')
+    let num3 = document.querySelector('#tres')
+
+}
+
+soma()
